e76312dbc3a9ccb8e7076da73fe83e7e3e1bde2f
lucas <aristoteles0727@gmail.com>
2025-01-28 22:05:33 -0300

    fixed path of dynamic route

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -11,11 +11,17 @@ async function GetProductData(id: string) {
     }
 }
 
-export default async function productPage() {
-    const productData = await GetProductData()
+export default async function productPage({ params }: {
+
+    params: Promise<{ id: string }>
+}) {
+    const id = (await params).id;
     return (
-        <div>
-            kakak
-        </div>
+        <div className="h-screen flex bg-blue-500 m-0 font-[family-name:var(--font-geist-sans)]">
+            { }
+            <div className="mt-24">
+                <h1 className="bg-blue-700 text-white">{id}</h1>
+            </div>
+        </div >
     )
 }
