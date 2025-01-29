f36aaefa3b1d30529a1e73236f97c2b4cbe2063b
lucas <aristoteles0727@gmail.com>
2025-01-29 13:49:16 -0300

    fixed link covering all card making the button unusable

--- src/components/cardProduct.tsx
+++ src/components/cardProduct.tsx
@@ -16,32 +16,36 @@ interface ProductCardProps {
 const ProductCard: React.FC<ProductCardProps> = ({ product, productLink }) => {
 
 
-    return (<Link href={productLink}>
+    return (
         <Card className="aspec-[3/4] hover:bg-secondary flex-col flex hover:z-10 transition-all duration-500 ease-in-out justify-center rounded-lg">
+            <Link href={productLink}>
+                <CardContent className="m-0">
 
-            <CardContent className="m-0">
+                    <Image
+                        className="sm:w-full h-full p-2 object-cover rounded-lg aspect-square"
+                        src={product.imageSrc}
+                        width={1280}
+                        height={2080}
+                        alt="image"
+                    />
 
-                <Image
-                    className="sm:w-full h-full p-2 object-cover rounded-lg aspect-square"
-                    src={product.imageSrc}
-                    width={1280}
-                    height={2080}
-                    alt="image"
-                />
+                </CardContent>
 
-            </CardContent>
-            <CardHeader className="p-3">
-                <CardTitle className="text-base h-[3em] font-semibold line-clamp-3">{product.title}</CardTitle>
-                <span className="text-sm mt-4 text-gray-600"><s>R$ {product.price}</s></span>
-                <span className="text-xl mt-0 text-primary font-semibold rounded py-1 bg-white justify-start mr-auto">R$ {product.price}</span>
-                <span className="line-clamp-1 mt-0 p-0 text-sm text-gray-500">A vista no PIX</span>
-                <span className="line-clamp-1 mt-0 text-sm text-gray-500">ou em até <b className="">10x de R$ {(parseInt(product.price) * 1.2) / 10}</b></span>
+                <CardHeader className="p-1">
 
-                <Button onClick={() => { console.log("didn' got redirected") }} className="mt-4"><ShoppingCart />Comprar</Button>
-            </CardHeader>
+                    <CardTitle className="text-base h-[3em] font-semibold line-clamp-3">{product.title}</CardTitle>
+                    <span className="text-sm mt-4 text-gray-600"><s>R$ {product.price}</s></span>
+                    <span className="text-xl mt-0 text-primary font-semibold rounded py-1 bg-white justify-start mr-auto">R$ {product.price}</span>
+                    <span className="line-clamp-1 mt-0 p-0 text-sm text-gray-500">A vista no PIX</span>
+                    <span className="line-clamp-1 mt-0 text-sm text-gray-500">ou em até <b className="">10x de R$ {(parseInt(product.price) * 1.2) / 10}</b></span>
+
+
+                </CardHeader>
+            </Link>
+            <Button onClick={() => { console.log("button clicked") }} className="m-1"><ShoppingCart />Comprar</Button>
 
         </Card>
-    </Link>
+
 
     )
 }
