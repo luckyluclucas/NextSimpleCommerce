7e2bfb8b66ccaa9ba085f4c887b9f940b1afe839
lucas <aristoteles0727@gmail.com>
2025-01-31 16:44:02 -0300

    fixing the functions to addCart to work properly

--- src/app/contexts/CartContextProvider.tsx
+++ src/app/contexts/CartContextProvider.tsx
@@ -2,19 +2,25 @@
 import CartContext from "@/app/contexts/cartContext";
 import { ReactNode, useState } from "react";
 import cartItem from "../types/cartItem";
+import { product } from "../types/product";
 
 export default function CartContextProvider({ children }: {
     children: ReactNode
 }) {
     const [cart, setCart] = useState<cartItem[]>([])
 
-    const addToCart = (product: cartItem) => {
+    const addToCart = (product: product) => {
         setCart((prevCart) => {
-            const cartHasProduct = prevCart.find((p) => p.id === product.id)
+            const cartHasProduct = prevCart.find((p) => p.productId === product.id)
             if (cartHasProduct) {
-                return prevCart.map((p) => p.id === product.id ? { ...p, quantity: p.quantity + 1 } : p)
+                return prevCart.map((p) => p.productId === product.id ? { ...p, quantity: p.quantity + 1 } : p)
             } else {
-                return [...prevCart, { ...product, quantity: 1 }]
+                const NewCartItem: cartItem = {
+                    productId: product.id,
+                    product: product,
+                    quantity: 1,
+                }
+                return [...prevCart, NewCartItem]
             }
 
         })
