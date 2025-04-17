0db99d032826e72801697cca57009294a32e1941
luckyluclucas <aristoteles0727@gmail.com>
2025-04-17 19:38:46 -0300

    separate the logic of getting data and properly renamed function to better clarity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,25 @@
-import Image from "next/image"
-import { product } from "./types/product"
-import CarouselWithProduct from "@/components/CarouselWithProducts"
+import Image from "next/image";
+import { product } from "./types/product";
+import CarouselWithProduct from "@/components/CarouselWithProducts";
+import { getProductData } from "@/database/getProductData";
 
-async function GetProducts(): Promise<product[]> {
-    try {
-        const data = await fetch(process.env.URL + "/api/products")
-        const products: product[] = await data.json()
-        return products
-    } catch (error) {
-        console.log("a error has ocurred while trying to fetch de products data")
-        console.log(error)
-        return []
-    }
-}
-
+export const revalidate = 60;
 
 export default async function Home() {
+  const data: product[] = await getProductData();
 
-    const products = await GetProducts()
-
-
-    return (
-        <div className="flex flex-col m-0 w-full">
-
-            <div className="w-full h-[400px] absolute flex mt-14">
-                <Image src="https://images.unsplash.com/photo-1491166617655-0723a0999cfc?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bmljZXxlbnwwfHwwfHx8MA%3D%3D"
-                    fill={true}
-                    alt="hahaah">
-                </Image >
-
-            </div>
-            <div className="flex flex-col p-4 z-10 mt-[466px]">
-                <CarouselWithProduct products={products} />
-            </div>
-
-
-        </div >
-    );
+  return (
+    <div className="flex flex-col m-0 w-full">
+      <div className="w-full h-[400px] absolute flex mt-14">
+        <Image
+          src="https://images.unsplash.com/photo-1491166617655-0723a0999cfc?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bmljZXxlbnwwfHwwfHx8MA%3D%3D"
+          fill={true}
+          alt="hahaah"
+        ></Image>
+      </div>
+      <div className="flex flex-col p-4 z-10 mt-[466px]">
+        <CarouselWithProduct products={data} />
+      </div>
+    </div>
+  );
 }
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,61 +1,37 @@
 import { AspectRatio } from "@/components/ui/aspect-ratio";
-import { product } from "../types/product";
 import ProductCard from "@/components/cardProduct";
 import slugify from "slugify";
-import "../globals.css"
+import "../globals.css";
+import { getProductData } from "@/database/getProductData";
 
-async function getProducts(): Promise<product[]> {
-  try {
-    const res = await fetch(process.env.URL + '/api/products'); // Requisição para a API Route (mesmo projeto)
-    if (!res.ok) {
-      // Tratar erros de requisição. Importante para evitar erros silenciosos.
-      console.error(`Erro ao buscar produtos: ${res.status} ${res.statusText}`);
-      return []; // ou lançar um erro: throw new Error('Erro ao buscar produtos');
-    }
-    const products: product[] = await res.json();
-    return products;
-  } catch (error) {
-    console.error("Erro na requisição:", error);
-    return []; // ou lançar um erro
-  }
-}
-
-
-
-export default async function Home() {
-
-  const products = await getProducts();
-
+export default function ProductsPage() {
+  const products = getProductData();
 
   return (
     <div className="p-0 mx-auto font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-0 row-start-2 items-center sm:items-start z-0">
         <div className="absolute inset-0">
-          <AspectRatio ratio={16 / 9} className="bg-[var(--background)]">
-
-          </AspectRatio>
+          <AspectRatio
+            ratio={16 / 9}
+            className="bg-[var(--background)]"
+          ></AspectRatio>
         </div>
         <div className="flex w-full flex-col p-0 mx-auto mt-24">
-
           <div className="w-full z-1">
             <div className="m-auto w-full flex flex-col px-4 max-w-7xl mx-auto justify-center z-1 rounded-lg">
-
-              <div className="m-auto w-full flex flex-col md:grid md:grid-cols-3 lg:grid-cols-5 justify-center h-full">{
-
-                products.map((product) => (
-
-                  <div key={product.id} className="sm:basis-1/3 py-1 mx-1 md:basis-1/3 lg:basis-1/5 justify-center items-center aspect-square rounded-xl">
-
-                    <ProductCard product={product} productLink={`/product/${product.id}/${slugify(product.title)}`} />
-
-
+              <div className="m-auto w-full flex flex-col md:grid md:grid-cols-3 lg:grid-cols-5 justify-center h-full">
+                {products.map((product) => (
+                  <div
+                    key={product.id}
+                    className="sm:basis-1/3 py-1 mx-1 md:basis-1/3 lg:basis-1/5 justify-center items-center aspect-square rounded-xl"
+                  >
+                    <ProductCard
+                      product={product}
+                      productLink={`/product/${product.id}/${slugify(product.title)}`}
+                    />
                   </div>
-
-                ))
-              }
-
+                ))}
               </div>
-
             </div>
           </div>
         </div>
