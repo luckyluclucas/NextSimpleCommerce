51c3b645d95e692ba3591a95b9c082f257c73766
luckyluclucas <aristoteles0727@gmail.com>
2025-04-17 22:44:51 -0300

    added trust host option to be able to run auth in any plataform

--- src/auth.ts
+++ src/auth.ts
@@ -1,26 +1,25 @@
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
-import type { Provider } from "next-auth/providers"
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import type { Provider } from "next-auth/providers";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [Google
+  providers: [Google],
 
-    ],
+  pages: {
+    signIn: "/signIn",
+  },
+  trustHost: true,
+});
 
-    pages: {
-        signIn: "/signIn"
-    }
-})
+const providers: Provider[] = [Google];
 
-const providers: Provider[] = [
-    Google,
-]
-
-export const providerMap = providers.map((provider) => {
+export const providerMap = providers
+  .map((provider) => {
     if (typeof provider === "function") {
-        const providerData = provider()
-        return { id: providerData.id, name: providerData.name }
+      const providerData = provider();
+      return { id: providerData.id, name: providerData.name };
     } else {
-        return { id: provider.id, name: provider.name }
+      return { id: provider.id, name: provider.name };
     }
-}).filter((provider) => provider.id !== "credentials")
+  })
+  .filter((provider) => provider.id !== "credentials");
