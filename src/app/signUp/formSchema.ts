6b8fff571ecd2608cb444cb27b7d0aae5ce91226
luckyluclucas <aristoteles0727@gmail.com>
2025-04-26 19:25:21 -0300

    formSchema update

--- src/app/signUp/formSchema.ts
+++ src/app/signUp/formSchema.ts
@@ -1,7 +1,10 @@
 import z from "zod";
 
 export const formSchema = z.object({
-  username: z.string().min(6).max(40),
-  email: z.string().min(5).email(),
-  password: z.string().min(12),
+  username: z
+    .string()
+    .min(6, "ya username should have at least 6 chars")
+    .max(40),
+  email: z.string().email("man, that's not an email"),
+  password: z.string().min(12, "password shoud have at least 12 chars"),
 });
