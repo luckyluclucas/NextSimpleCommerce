afdf7630abb3422bad40bd6e9b248f4bd92ca96b
luckyluclucas <aristoteles0727@gmail.com>
2025-04-29 01:49:32 -0300

    fix stupidity

--- src/auth.ts
+++ src/auth.ts
@@ -4,9 +4,7 @@ import Google from "next-auth/providers/google";
 import type { Provider } from "next-auth/providers";
 import PostgresAdapter from "@auth/pg-adapter";
 import pool from "./app/database/pool";
-import { getProductData } from "./app/database/db";
 import Credentials from "next-auth/providers/credentials";
-getProductData();
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PostgresAdapter(pool),
