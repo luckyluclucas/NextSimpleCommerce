5c2d53e6f8361ce8856346c9d0ec162443dd61db
luckyluclucas <aristoteles0727@gmail.com>
2025-04-29 01:48:37 -0300

    made titles longer

--- src/app/database/tools/seedTheDatabaseWithFakeProducts.js
+++ src/app/database/tools/seedTheDatabaseWithFakeProducts.js
@@ -12,9 +12,10 @@ export async function seedFakeProductsOnDatabase(TotalNumberOfproducts) {
   function CreateFakeProduct(TotalNumberOfproducts) {
     for (let i = 0; i < TotalNumberOfproducts; i++) {
       const product = {
-        title: faker.commerce.productName(),
+        title:
+          faker.commerce.productName() + " " + faker.commerce.productName(),
         description: faker.commerce.productDescription(),
-        price: faker.commerce.price({ min: 100, max: 10000 }),
+        price: faker.commerce.price({ min: 700, max: 1000 }),
         stock: Math.floor(Math.random() * 100),
         image:
           "https://images.unsplash.com/photo-1722888061836-b3bae5fc5320?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
